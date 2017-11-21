1f9054e7711a5fbed2f6b4cdfbdf3bdd4cba136c
connorlu <connorlu@tencent.com>
2017-11-21 16:16:56 +0800

    提交下拉刷新和下一页相关的逻辑。

--- index.js
+++ index.js
@@ -25,45 +25,145 @@ index.get('/', function (req, res) {
     res.send('Hello World');
 })
 
+var VNChannelDataFirstPageDateIsOdd = false;
+
 index.get('/data/VNChannelData', function (req, res) {
-    var channelData = {
-        "errCode": 0,
-        "hasNextPage": false,
-        "pageContext": "_1",
-        "listData": [{
-            "cellType": "bigPoster",
-            "url": "http://puui.qpic.cn/tv/0/16195733_1080607/0",
-            "firstLine": "蓝色星球 第二季",
-            "secondLine": "展示简易版视频首屏实现，包括下拉刷新，分页加载，换一批等功能"
-        }, {
-            "cellType": "timePoster",
-            "firstLine": "VN为您报时",
-            "secondLine": new Date().toTimeString()
-        }, {
-            "cellType": "titlePoster",
-            "firstLine": "精彩电视剧"
-        }, {
-            "cellType": "contentPoster",
-            "url": "http://puui.qpic.cn/vpic/0/o0500ekuesl.png/0",
-            "firstLine": "前几天订购的奔驰到货，掀开车布后感觉一切都值得！"
-        }, {
-            "cellType": "contentPoster",
-            "url": "http://puui.qpic.cn/vpic/0/x05655hyc2l.png/0",
-            "firstLine": "李连杰演坏人，最帅的一次！"
-        }, {
-            "cellType": "contentPoster",
-            "url": "http://puui.qpic.cn/vpic/0/z0562zvchuh.png/0",
-            "firstLine": "《战狼3》龙小云强势回归，冷锋会怎么选？"
-        }, {
-            "cellType": "contentPoster",
-            "url": "http://puui.qpic.cn/vpic/0/d0504ccvu7d.png/0",
-            "firstLine": "要不是有人拍下了视频，你就是说破天也没人相信这是真的"
-        }, {
-            "cellType": "contentPoster",
-            "url": "http://puui.qpic.cn/vpic/0/i0549lql5qf.png/0",
-            "firstLine": "超能对决，雷神大战超人，雷神高傲被痛扁"
-        }]
-    };
+    var pageContext = req.query.pageContext;
+    VNChannelDataFirstPageDateIsOdd = !VNChannelDataFirstPageDateIsOdd;
+
+    var channelData = null;
+    console.log(pageContext);
+
+    if ((!pageContext || 0 === pageContext.length)) {
+        if (VNChannelDataFirstPageDateIsOdd) {
+            channelData = {
+                "errCode": 0,
+                "hasNextPage": true,
+                "pageContext": "_1",
+                "listData": [{
+                    "cellType": "bigPoster",
+                    "url": "http://puui.qpic.cn/tv/0/16195733_1080607/0",
+                    "firstLine": "蓝色星球 第二季",
+                    "secondLine": "展示简易版视频首屏实现，包括下拉刷新，分页加载，换一批等功能"
+                }, {
+                    "cellType": "timePoster",
+                    "firstLine": "VN为您报时",
+                    "secondLine": new Date().toTimeString()
+                }, {
+                    "cellType": "titlePoster",
+                    "firstLine": "精彩电视剧"
+                }, {
+                    "cellType": "contentPoster",
+                    "url": "http://puui.qpic.cn/vpic/0/o0500ekuesl.png/0",
+                    "firstLine": "前几天订购的奔驰到货，掀开车布后感觉一切都值得！"
+                }, {
+                    "cellType": "contentPoster",
+                    "url": "http://puui.qpic.cn/vpic/0/x05655hyc2l.png/0",
+                    "firstLine": "李连杰演坏人，最帅的一次！"
+                }, {
+                    "cellType": "contentPoster",
+                    "url": "http://puui.qpic.cn/vpic/0/z0562zvchuh.png/0",
+                    "firstLine": "《战狼3》龙小云强势回归，冷锋会怎么选？"
+                }, {
+                    "cellType": "contentPoster",
+                    "url": "http://puui.qpic.cn/vpic/0/d0504ccvu7d.png/0",
+                    "firstLine": "要不是有人拍下了视频，你就是说破天也没人相信这是真的"
+                }, {
+                    "cellType": "contentPoster",
+                    "url": "http://puui.qpic.cn/vpic/0/i0549lql5qf.png/0",
+                    "firstLine": "超能对决，雷神大战超人，雷神高傲被痛扁"
+                }]
+            };
+        } else {
+            channelData = {
+                "errCode": 0,
+                "hasNextPage": true,
+                "pageContext": "_1",
+                "listData": [{
+                    "cellType": "bigPoster",
+                    "url": "http://puui.qpic.cn/tv/0/16190755_330185/0",
+                    "firstLine": "龙猫 第一季",
+                    "secondLine": "短尾毛丝鼠体型较大，体长30～38cm，尾长10cm左右；长尾毛丝鼠体型较小，体长24～28cm，尾长14～15cm。 一般雌鼠体重510～710g，雄鼠体重425～570g。前半身似兔，后半身似鼠，耳大钝圆，尾毛蓬松。眼睛明亮，鼻端两侧有许多长须。标准毛丝鼠皮毛呈蓝灰色，腹部渐淡至白色，腹中部有分界明显的白色带。"
+                }, {
+                    "cellType": "timePoster",
+                    "firstLine": "VN为您报时",
+                    "secondLine": new Date().toTimeString()
+                }, {
+                    "cellType": "titlePoster",
+                    "firstLine": "精彩综艺"
+                }, {
+                    "cellType": "contentPoster",
+                    "url": "http://puui.qpic.cn/tv/0/16200462/0",
+                    "firstLine": "VideoNative功能简介"
+                }, {
+                    "cellType": "contentPoster",
+                    "url": "http://i.gtimg.cn/qqlive/images/20160715/only@2x.png",
+                    "firstLine": "李连杰演坏人，最帅的一次！"
+                }, {
+                    "cellType": "contentPoster",
+                    "url": "http://puui.qpic.cn/tv/0/16140838_680382/0",
+                    "firstLine": "《战狼3》龙小云强势回归，冷锋会怎么选？"
+                }, {
+                    "cellType": "contentPoster",
+                    "url": "http://puui.qpic.cn/vpic/0/d0504ccvu7d.png/0",
+                    "firstLine": "要不是有人拍下了视频，你就是说破天也没人相信这是真的"
+                }, {
+                    "cellType": "contentPoster",
+                    "url": "http://puui.qpic.cn/vpic/0/i0549lql5qf.png/0",
+                    "firstLine": "超能对决，雷神大战超人，雷神高傲被痛扁"
+                }]
+            };
+        }
+    } else {
+        channelData = {
+            "errCode": 0,
+            "hasNextPage": false,
+            "pageContext": "_2",
+            "listData": [{
+                "cellType": "titlePoster",
+                "firstLine": "这是第二页数据"
+            }, {
+                "cellType": "bigPoster",
+                "firstLine": "魔鬼鱼一言不合就上天",
+                "secondLine": "水中上演“荧光芭蕾”",
+                "url": "http://puui.qpic.cn/tv/0/16040491_498280/0"
+              },
+              {
+                "cellType": "contentPoster",
+                "firstLine": "虎鲸用尾巴抽打鱼群",
+                "secondLine": "史上最强“巴掌”",
+                "url": "http://puui.qpic.cn/tv/0/16040532_498280/0"
+              },
+              {
+                "cellType": "contentPoster",
+                "firstLine": "伪虎鲸群围猎海豚",
+                "secondLine": "结局逆转太意外",
+                "url": "http://puui.qpic.cn/tv/0/16040565_498280/0"
+              },
+              {
+                "cellType": "contentPoster",
+                "firstLine": "总是吃不饱的海参",
+                "secondLine": "大胃王！看起来好饿",
+                "url": "http://puui.qpic.cn/tv/0/16070266_498280/0"
+              },
+              {
+                "cellType": "contentPoster",
+                "firstLine": "这种鱼长着长着变性了",
+                "secondLine": "而且还是一夫多妻制！",
+                "url": "http://puui.qpic.cn/tv/0/16040505_498280/0"
+              },
+              {
+                "cellType": "contentPoster",
+                "firstLine": "大鱼跃出水面捕食飞鸟",
+                "secondLine": "这鱼太挑食了！",
+                "url": "http://puui.qpic.cn/tv/0/16040463_498280/0"
+              }]
+        };
+    }
+
+
+
+
 
     res.json(channelData);
 })
